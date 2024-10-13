f567fdccda5833084f13fdbcb1d068d0a6e9a05c
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-10-13 17:24:47 +0200

    Redirect to dashboard

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+
+import { useRouter } from "@/navigation";
 
 export default function Index() {
+  const router = useRouter();
   const indexPageText = useTranslations("Index");
+
+  useEffect(() => {
+    // Redirect to the dashboard page
+    router.push("/dashboard");
+  }, [router]);
 
   return (
     <main className="relative">
