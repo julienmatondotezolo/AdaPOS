e60801dbac4fca791b37265746594f41579f174c
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-10-14 18:44:22 +0200

    refactor switch darkmode

--- src/app/[locale]/(pages)/layout.tsx
+++ src/app/[locale]/(pages)/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import React, { useEffect } from "react";
+import React from "react";
 
 import Providers from "../providers";
 
@@ -12,16 +11,6 @@ export default function PagesLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const { setTheme } = useTheme();
-
-  useEffect(() => {
-    // Set the theme based on user preference or system preference
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    prefersDark;
-    setTheme("dark");
-  }, [setTheme]);
-
   return (
     <Providers locale={locale}>
       <main className="h-[100vh] w-[100vw] overflow-hidden">{children}</main>
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
-import { ArrowLeft, Search } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
+import { useTheme } from "next-themes";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -14,6 +15,12 @@ function Navigation() {
     dispatch(removeTable([]));
   };
 
+  const { setTheme, theme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark"); // Toggle theme based on current theme
+  };
+
   return (
     <div className="w-full border-2 border-neutral-900">
       <section className="w-full md:w-9/12 p-4 flex justify-between items-center">
@@ -26,9 +33,12 @@ function Navigation() {
             <small className="text-xs font-medium">{isTableSelected ? "16 tables" : "Items"}</small>
           </section>
         </article>
-        <form className="flex cursor-pointer">
+        {/* <form className="flex cursor-pointer">
           <Search size={20} />
-        </form>
+        </form> */}
+        <button onClick={toggleTheme} className="ml-4 p-2 border rounded">
+          {theme === "dark" ? "Switch" : "Switch"}
+        </button>
       </section>
     </div>
   );
